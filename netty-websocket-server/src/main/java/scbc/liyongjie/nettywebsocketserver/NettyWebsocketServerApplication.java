c25d52cc8a132199ee78b1b0a8ffe7288fb6dbd5
CoolCo <893295815@qq.com>
2022-03-08 00:44:40 +0800

    3.8 12:44

---
+++ netty-websocket-server/src/main/java/scbc/liyongjie/nettywebsocketserver/NettyWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package scbc.liyongjie.nettywebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyWebsocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyWebsocketServerApplication.class, args);
+    }
+
+}
---
+++ service-ffmpeg-api/src/main/java/com/example/serviceffmpegapi/ServiceFfmpegApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceffmpegapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFfmpegApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFfmpegApiApplication.class, args);
+    }
+
+}
